c485c766090a791533c906012c31a34cc412bfac
Dusan Trtica <dusantrtica@M-CJK94RXMVC.local>
2025-03-23 14:09:49 +0100

    switch to hooks for text search

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import words from '../data/words.json';
 import { Trie } from "../algorithms/prefix-tree";
+import { useTextFilter } from "./TextFilter";
 
 const trie = new Trie();
 
@@ -19,7 +20,7 @@ const WordsWithPrefix = ({ words, prefix }: { words: string[], prefix: string })
     );
 }
 
-const WordsWithPrefixTrie = ({trie, prefix}:{ trie: Trie, prefix: string}) => {
+const WordsWithPrefixTrie = ({ trie, prefix }: { trie: Trie, prefix: string }) => {
     const start = performance.now();
     const filteredWords = trie.wordsWithPrefix(prefix);
     const end = performance.now();
@@ -41,8 +42,11 @@ const Word = ({ word }: { word: string }) => (
 const Autocomplete = () => {
     const [searchValue, setSearchValue] = useState<string>("");
 
+    const { result, onInputChange } = useTextFilter(words);
+
     const handleInputChange = (e: any) => {
         setSearchValue(e.target.value);
+        onInputChange(e.target.value)
     }
 
     useEffect(() => {
@@ -50,14 +54,19 @@ const Autocomplete = () => {
         console.log("words inserted into trie");
     }, [])
 
+
+
     return (
-        <div>
+        <>
             <input onChange={handleInputChange} className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow" placeholder="Type here..." />
             <div className="flex flex-row justify-between mt-4 gap-4">
                 <WordsWithPrefix words={words} prefix={searchValue} />
                 <WordsWithPrefixTrie trie={trie} prefix={searchValue} />
-            </div>            
-        </div>
+                <div>
+                    {result.map(word => <Word word={word} key={word} />)}
+                </div>
+            </div>
+        </>
     )
 }
 
